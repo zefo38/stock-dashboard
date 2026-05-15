89b3fdd94d78659c7a15c407f9b5ba112368a4d8
zefo38 <hicg38@naver.com>
2026-05-15 14:37:36 +0900

    잔고조회, 현재가조회 함수 추가

--- kis_api.py
+++ kis_api.py
@@ -23,7 +23,7 @@ def get_access_token():
     url = f"{BASE_URL}/oauth2/tokenP"
     headers = {"Content-Type": "application/json"}
     body = {
-        "grant_type": client_credentials",
+        "grant_type": "client_credentials",
         "appkey": APP_KEY,
         "appsecret": APP_SECRET
     }
@@ -34,3 +34,48 @@ def get_access_token():
 
 # 계좌 잔고 조회
 def get_stock_balance(token: str) -> dict : 
+    """주식 잔고 조회"""
+    url = f"{BASE_URL}/uapi/domestic-stock/v1/trading/inquire-balance"
+    headers = {
+        "content-type":"application/json",
+        "authorization":f"Bearer {token}",
+        "appkey": APP_KEY,
+        "appsecret": APP_SECRET,
+        "tr_id":"VTTC8434R" if IS_MOCK else "TTTC8434R"  # 모의투자와 실전투자 트랜잭션 ID가 다름
+    }
+    params = {
+        "CANO": ACCOUNT_NO,
+        "ACNT_PRDT_CD": ACCOUNT_PROD_CODE,
+        "AFHR_FLPR_YN": "N",
+        "OFL_YN": "N",
+        "INQR_DVSN": "02",
+        "UNPR_DVSN": "01",
+        "FUND_STTL_ICLD_YN": "N",
+        "FNCG_AMT_AUTO_RDPT_YN": "N",
+        "PRCS_DVSN": "01",
+        "CTX_AREA_FK100": "",
+        "CTX_AREA_NK100": ""
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+
+def get_stock_price(token: str, stock_code: str) -> dict:
+    """개별 주식 현재가 조회"""
+    url = f"{BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-price"
+    headers = {
+        "content-type":"application/json",
+        "authorization":f"Bearer {token}",
+        "appkey": APP_KEY,
+        "appsecret": APP_SECRET,
+        "tr_id":"FHKST01010100",
+    }
+    params = {
+        "FID_COND_MRKT_DIV_CODE": "J",  # 주식
+        "FID_INPUT_ISCD": stock_code
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
